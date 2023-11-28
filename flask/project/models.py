67d8999487ae2a607d3085cc837545bc80fdfc58
galjakub <you@example.com>
2023-11-28 19:30:48 +0100

    Initial commit

---
+++ flask/project/models.py
@@ -0,0 +1,12 @@
+from project.extensions import db
+
+class Flat(db.Model):
+    __tablename__ = "flats"
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255), unique=False, nullable=False)
+    image_url = db.Column(db.String(255), unique=False, nullable=True)
+
+    def __init__(self, title, image_url):
+        self.title = title
+        self.image_url = image_url
